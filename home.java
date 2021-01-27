bdfc4be9951b79b19b07fb2d1de6d8f686650c3b
root <root@ariyath-VirtualBox.(none)>
2021-01-27 11:38:11 +0530

    my frist project

---
+++ home.java
@@ -0,0 +1,7 @@
+class Home{
+          public static void main(string []agrs)
+          {
+          system.out.println("hello from Home file");
+          }
+}
+
---
+++ login.java
@@ -0,0 +1,6 @@
+class Login {
+            public static void main(sting []agrs)
+           {
+            system.out.println("hello from login file");
+}
+}
